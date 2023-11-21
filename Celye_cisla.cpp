db1edcf0666dbc45c77c2384d371e62efa77c91d
Alexander Podkosov <150276248+AlexPodkosov@users.noreply.github.com>
2023-11-21 10:19:04 +0300

    Add files via upload

---
+++ Celye_cisla.cpp
@@ -0,0 +1,24 @@
+﻿// Celye_cisla.cpp : Найти произведение всех целых чисел от a до 20.
+// (значение а вводится с клвиатуры 1<=a<=20)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a;
+   long long pChisel = 1;
+
+    cout << "Vvedite chislo s klaviatury (1 <= a <= 20) = "; 
+    cin >> a;
+
+    for (int i = a; i <= 20; i++)
+    {
+            pChisel = pChisel * i;
+    }
+
+    cout << " Proizvedenie vseh celych chicel: " << pChisel << endl;
+
+    return 0;
+}
+
