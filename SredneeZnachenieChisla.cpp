cdaf140dedd5d279e43f9d504732f17c301df146
Alexander Podkosov <150276248+AlexPodkosov@users.noreply.github.com>
2023-11-23 11:15:31 +0300

    Add files via upload

---
+++ SredneeZnachenieChisla.cpp
@@ -0,0 +1,24 @@
+﻿// Задание 1. Найти среднее значение чисел.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    
+    cout << "Enter a :";
+    cin >> a;
+
+    cout << "Enter b :";
+    cin >> b;
+
+    cout << "Enter c :";
+    cin >> c;
+
+    int Srednee_znachenie = (a + b + c) / 3;
+    cout << " Srednee_znachenie = " << Srednee_znachenie << "\n";
+
+    return 0;
+}
