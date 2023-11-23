cc5ffbe9d76c243a8491fe4fb9f19e202dcb1e02
Alexander Podkosov <150276248+AlexPodkosov@users.noreply.github.com>
2023-11-23 11:03:36 +0300

    Add files via upload

---
+++ Sea_battle.cpp
@@ -0,0 +1,20 @@
+﻿// Sea_battle.cpp : Вывести в консоль поля игры морской бой.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+			cout << char('A' + j) << i << " ";
+		if (i < 9)
+		{
+			cout << endl;
+		}
+	}
+	return 0;
+}
+
+
